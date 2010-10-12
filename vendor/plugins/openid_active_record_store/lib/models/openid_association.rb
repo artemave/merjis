7e007bc836b2340b4e057912f5fcf74be7f3d1ea
artemave <artemave@gmail.com>
2010-10-12 16:07:31 +0000

    trying workaraond for PGError: ERROR: invalid byte sequence for encoding "UTF8"

--- vendor/plugins/openid_active_record_store/lib/models/openid_association.rb
+++ vendor/plugins/openid_active_record_store/lib/models/openid_association.rb
@@ -1,2 +1,9 @@
 class OpenidAssociation < OpenidAbstract
+  def secret
+    Base64.decode64 read_attribute(:secret)
+  end
+
+  def secret=(s)
+    write_attribute(:secret, Base64.encode64(s))
+  end
 end
