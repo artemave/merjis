e6ba23ff064c09061a4cd15b7349e2db6d57e5a9
artemave <artemave@gmail.com>
2010-10-27 10:50:32 +0100

    website categories added to ui

--- app/models/website.rb
+++ app/models/website.rb
@@ -2,7 +2,7 @@ require 'custom_validators'
 
 class Website < ActiveRecord::Base
 
-  attr_accessible :fqdn, :name, :description
+  attr_accessible :fqdn, :name, :description, :main, :video, :images, :mobile
 
   belongs_to :user
   has_many :shares, :as => :resource
@@ -14,4 +14,8 @@ class Website < ActiveRecord::Base
   validates :fqdn,
     :presence => true,
     :uniqueness => {:scope => :user_id}
+
+  def categories
+    [:main, :images, :video, :mobile].select{ |c| send(c) }
+  end
 end
